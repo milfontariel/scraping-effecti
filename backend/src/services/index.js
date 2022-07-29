b78ffc0613103ff3f93565342dbe2699572ffc00
ariel <ariel1242009@gmail.com>
2022-07-29 02:04:28 -0300

    feat: load bidding

--- backend/src/services/index.js
+++ backend/src/services/index.js
@@ -1,17 +1,13 @@
 import puppeteer from "puppeteer";
-import dotenv from 'dotenv';
 import fs from 'fs';
 
-
-
-
-(async () => {
+export async function getBiddingsWithKeyword(busca) {
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
     await page.goto('http://www.recife.pe.gov.br/portalcompras/app/Licitacoes.php');
     await page.goto('http://www.recife.pe.gov.br/portalcompras/app/ConsLicitacoesAndamento.php');
 
-    await page.evaluate(() => {
+    await page.evaluate((busca) => {
         function enviar(valor, botao, texto) {
             document.querySelector("#txtObjeto").value = texto;
             var valorForm = $('.formSubmit').val();
@@ -19,15 +15,23 @@ import fs from 'fs';
             $('form[name=' + valorForm + ']').submit();
         }
 
-        enviar('Pesquisa', 'Botao', '');
-    })
+        enviar('Pesquisa', 'Botao', busca);
+    }, busca);
 
-    await page.waitForSelector("body > div > div.site-body > div > div.content > div > div > section > div > section:nth-child(5) > table");
+    try {
+        await page.waitForSelector(
+            "body > div > div.site-body > div > div.content > div > div > section > div > section:nth-child(5) > table", {
+            timeout: 5000,
+            visible: true
+        });
+    } catch (error) {
+        await browser.close();
+        return 'nada encontrado'
+    }
 
     const response = () => {
         return page.evaluate(async () => {
             return await new Promise((resolve) => {
-                const items = document.body.querySelectorAll(`body > div > div.site-body > div > div.content > div > div > section > div > section:nth-child(5) > table > tbody tr:last-child`);
                 const array = Array.from(document.body.querySelectorAll(`body > div > div.site-body > div > div.content > div > div > section > div > section:nth-child(5) > table > tbody tr:last-child`), e => ({
                     titulo: e.children[0].innerText,
                     ref: e.children[0].children[0].href.replace("javascript:carregaProcesso('", '').replace("');", ''),
@@ -40,34 +44,80 @@ import fs from 'fs';
                     numSolicitacaoCompra: e.children[7].innerText,
                     valorEstimado: e.children[8].innerText,
                 }));
-                console.log(items);
                 resolve(array);
             })
         })
     }
 
 
-    const teste = await response();
-    console.log(teste);
+    const result = await response();
 
-
-    /* const htmlObject = document.createElement('table');
-    htmlObject.innerHTML = teste; */
-
-    fs.writeFile('teste.json', JSON.stringify(teste, null, 2), err => {
+    fs.writeFile('result.json', JSON.stringify(result, null, 2), err => {
         if (err) throw err;
         console.log('File is created successfully.');
     });
 
+    await browser.close();
+    return result;
+};
 
-    // ******* Funções do documento
+export async function loadBidding(ref) {
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+    await page.goto('http://www.recife.pe.gov.br/portalcompras/app/Licitacoes.php');
+    await page.goto('http://www.recife.pe.gov.br/portalcompras/app/ConsLicitacoesAndamento.php');
 
+    await page.evaluate(() => {
+        function enviar(valor, botao, texto) {
+            document.querySelector("#txtObjeto").value = texto;
+            var valorForm = $('.formSubmit').val();
+            $('input[name=' + botao + ']').val(valor);
+            $('form[name=' + valorForm + ']').submit();
+        }
+        enviar('Pesquisa', 'Botao', '');
+    });
 
-    function carregaProcesso(valor) {
-        document.Acomp.Botao.value = 'carregaProcesso';
-        document.Acomp.carregaProcesso.value = valor;
-        document.Acomp.submit();
+    try {
+        await page.waitForSelector(
+            "body > div > div.site-body > div > div.content > div > div > section > div > section:nth-child(5) > table", {
+            timeout: 5000,
+            visible: true
+        });
+    } catch (error) {
+        await browser.close();
+        return 'nada encontrado'
     }
 
-    // await browser.close();
-})();
+    await page.evaluate((ref) => {
+        function carregaProcesso(valor) {
+            document.Acomp.Botao.value = 'carregaProcesso';
+            document.Acomp.carregaProcesso.value = valor;
+            document.Acomp.submit();
+        }
+        carregaProcesso(ref);
+    }, ref);
+
+    await page.waitForSelector("body > div > div.site-body > div > div.content > div > div > section > div > article:nth-child(3) > section:nth-child(2) > table");
+
+    const response = () => {
+        return page.evaluate(async () => {
+            return await new Promise((resolve) => {
+                const html = document.querySelector("body > div > div.site-body > div > div.content > div > div > section > div > article:nth-child(3) > section:nth-child(2) > table").outerHTML;
+                resolve(html)
+            })
+        })
+    }
+
+    const result = await response();
+
+    fs.writeFile('result.html', result, err => {
+        if (err) throw err;
+        console.log('File is created successfully.');
+    });
+
+    await browser.close();
+
+    return result;
+}
+
+loadBidding('1-25-2022-1-6')
